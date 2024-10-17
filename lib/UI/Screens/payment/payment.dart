285fd677be2033cbd192a069a32a607f986ebbd3
Muhammad Adnan11 <adnankhalil984@gmail.com>
2024-10-17 11:44:13 -0700

    more addtition

--- lib/UI/Screens/payment/payment.dart
+++ lib/UI/Screens/payment/payment.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+
 class Payment extends StatefulWidget {
   const Payment({super.key});
 
@@ -8,6 +9,15 @@ class Payment extends StatefulWidget {
 }
 
 class _PaymentState extends State<Payment> {
+  // List of image paths
+  final List<String> imgList = [
+    'assets/images/card.jpeg',
+    'assets/images/card1.png',
+    'assets/images/card2.png',
+    'assets/images/card3.jpeg',
+    'assets/images/card4.jpeg',
+    'assets/images/card5.jpeg',
+  ];
   @override
   Widget build(BuildContext context) {
     var heigthX = MediaQuery.of(context).size.height;
@@ -23,18 +33,23 @@ class _PaymentState extends State<Payment> {
           children: [
             SizedBox(
               height: heigthX * 0.25,
-              child: PageView(
-                controller: PageController(
-                  initialPage: 0,
+              child: CarouselSlider.builder(
+                itemCount: imgList.length,
+                itemBuilder: (context, index, realIndex) {
+                  return Image.asset(
+                    imgList[index],
+                    fit: BoxFit.cover,
+                    width: MediaQuery.of(context).size.width,
+                  );
+                },
+                options: CarouselOptions(
+                  autoPlay: true, // Enable auto-slide
+                  autoPlayInterval: Duration(seconds: 3), // Set auto-slide interval
+                  enlargeCenterPage: true, // Enlarge the center page
+                  enlargeFactor: 0.3, // Control the enlargement factor of the center image
+                  aspectRatio: 2.0, // Aspect ratio of the carousel
+                  viewportFraction: 0.8,
                 ),
-                children: [
-                  Image.asset('assets/images/card.jpeg'),
-                  Image.asset('assets/images/card1.png'),
-                  Image.asset('assets/images/card2.png'),
-                  Image.asset('assets/images/card3.jpeg'),
-                  Image.asset('assets/images/card4.jpeg'),
-                  Image.asset('assets/images/card5.jpeg'),
-                ],
               ),
             ),
             Padding(
